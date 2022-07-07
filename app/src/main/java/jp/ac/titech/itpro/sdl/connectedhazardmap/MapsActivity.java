f6c841459261390992cd087379b05ad11ddd09a1
Takayoshi Ueno <takakun.0907050301@gmail.com>
2022-07-08 03:14:12 +0900

    feat: add my location button

--- app/src/main/java/jp/ac/titech/itpro/sdl/connectedhazardmap/MapsActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/connectedhazardmap/MapsActivity.java
@@ -1,34 +1,117 @@
 package jp.ac.titech.itpro.sdl.connectedhazardmap;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import jp.ac.titech.itpro.sdl.connectedhazardmap.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private final static String TAG = MapsActivity.class.getSimpleName();
 
-    private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+    private final static String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private final static int REQ_PERMISSIONS = 1234;
+
+    private GoogleMap map;
+
+    private FusedLocationProviderClient locationClient;
+    private LocationRequest request;
+    private LocationCallback callback;
+
+    private Location latestMyLocation = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_maps);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            Log.d(TAG, "onCreate: getMapAsync");
+            mapFragment.getMapAsync(this);
+        }
+
+        ImageButton myLocationButton = findViewById(R.id.myLocationButton);
+        myLocationButton.setOnClickListener((view) -> {
+            Log.d(TAG, "myLocationButton.onClick");
+            displayLatestMyLocation();
+        });
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        request = LocationRequest.create();
+        request.setInterval(10000L);
+        request.setFastestInterval(5000L);
+        request.setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        callback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Log.d(TAG, "onLocationResult");
+                if (latestMyLocation != null) {
+                    latestMyLocation = locationResult.getLastLocation();
+                    return;
+                }
+
+                // display user's location automatically only the first time
+                latestMyLocation = locationResult.getLastLocation();
+                displayLatestMyLocation();
+            }
+        };
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        startLocationUpdate(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        stopLocationUpdate();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
     }
 
     /**
@@ -41,12 +124,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady");
+        this.map = googleMap;
+        map.moveCamera(CameraUpdateFactory.zoomTo(15f));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdate(boolean reqPermission) {
+        Log.d(TAG, "startLocationUpdate");
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                if (reqPermission) {
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, REQ_PERMISSIONS);
+                } else {
+                    String text = getString(R.string.toast_requires_permission_format, permission);
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+        locationClient.requestLocationUpdates(request, callback, Looper.getMainLooper());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int reqCode, @NonNull String[] permissions, @NonNull int[] grants) {
+        Log.d(TAG, "onRequestPermissionsResult");
+        if (reqCode == REQ_PERMISSIONS) {
+            startLocationUpdate(false);
+        } else {
+            super.onRequestPermissionsResult(reqCode, permissions, grants);
+        }
+    }
+
+    private void stopLocationUpdate() {
+        Log.d(TAG, "stopLocationUpdate");
+        locationClient.removeLocationUpdates(callback);
+    }
+
+    private void displayLatestMyLocation() {
+        if (latestMyLocation == null) {
+            Log.d(TAG, "displayLatestMyLocation: latestMyLocation == null");
+            return;
+        }
+        LatLng ll = new LatLng(latestMyLocation.getLatitude(), latestMyLocation.getLongitude());
+        if (map == null) {
+            Log.d(TAG, "displayLatestMyLocation: map == null");
+            return;
+        }
+        map.animateCamera(CameraUpdateFactory.newLatLng(ll));
     }
 }
